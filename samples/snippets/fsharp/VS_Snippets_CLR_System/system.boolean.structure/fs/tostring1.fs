182b379d28d41ba4c8b8ad9f08eadbcc7fbe742d
Perrytheplaty43 <59098820+Perrytheplaty43@users.noreply.github.com>
2021-11-24 15:01:14 -0800

    Adding comments

--- samples/snippets/fsharp/VS_Snippets_CLR_System/system.boolean.structure/fs/tostring1.fs
+++ samples/snippets/fsharp/VS_Snippets_CLR_System/system.boolean.structure/fs/tostring1.fs
@@ -1,5 +1,10 @@
+// <Snippet3>
 let raining = false
 let busLate = true
 
 printfn "It is raining: %b" raining
 printfn "It is raining: %b" busLate
+// The example displays the following output:
+//       It is raining: False
+//       The bus is late: True
+// </Snippet3>
